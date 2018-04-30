47f7e39480f8a675fdbabce6b16263a498a243b9
SirMarkNL <mark@debruijnemail.nl>
2018-04-30 17:16:06 +0200

    Changed mecanojob to mechanicjob and translated SQL to EN

--- localization/esx_mecanojob_en.sql
+++ localization/esx_mecanojob_en.sql
@@ -1,30 +1,30 @@
 USE `essentialmode`;
 
 INSERT INTO `addon_account` (name, label, shared) VALUES
-	('society_mecano', 'Mechanic', 1)
+	('society_mechanic', 'Mechanic', 1)
 ;
 
 INSERT INTO `addon_inventory` (name, label, shared) VALUES
-	('society_mecano', 'Mechanic', 1)
+	('society_mechanic', 'Mechanic', 1)
 ;
 
 INSERT INTO `jobs` (name, label) VALUES
-	('mecano', 'Mechanic')
+	('mechanic', 'Mechanic')
 ;
 
 INSERT INTO `job_grades` (job_name, grade, name, label, salary, skin_male, skin_female) VALUES
-	('mecano',0,'recrue','Recruit',12,'{}','{}'),
-	('mecano',1,'novice','Novice',24,'{}','{}'),
-	('mecano',2,'experimente','Experienced',36,'{}','{}'),
-	('mecano',3,'chief','Leader',48,'{}','{}'),
-	('mecano',4,'boss','Boss',0,'{}','{}')
+	('mechanic',0,'recrute','Recrute',12,'{}','{}'),
+	('mechanic',1,'novice','Rookie',24,'{}','{}'),
+	('mechanic',2,'experienced','Experienced',36,'{}','{}'),
+	('mechanic',3,'chief','Chief',48,'{}','{}'),
+	('mechanic',4,'boss','Director',0,'{}','{}')
 ;
 
 INSERT INTO `items` (name, label, limit) VALUES
-	('gazbottle', 'Gas Bottle', 11),
-	('fixtool', 'Repair Tools', 6),
-	('carotool', 'Tools', 4),
-	('blowpipe', 'Blowtorch', 10),
+	('gazbottle', 'Gas can', 11),
+	('fixtool', 'Repair kit', 6),
+	('carotool', 'Bodywork Tool', 4),
+	('blowpipe', 'Blowpipe', 10),
 	('fixkit', 'Repair Kit', 5),
-	('carokit', 'Body Kit', 3)
+	('carokit', 'Bodykit', 3)
 ;
